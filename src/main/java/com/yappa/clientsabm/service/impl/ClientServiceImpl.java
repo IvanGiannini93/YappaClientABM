9b5de539d6ca65f1e7b90e4564bb086738c3b2c9
701792 <701792@host.docker.internal>
2025-11-06 00:30:35 -0300

    YAP-003: create and implements Client Service

---
+++ src/main/java/com/yappa/clientsabm/service/impl/ClientServiceImpl.java
@@ -0,0 +1,49 @@
+package com.yappa.clientsabm.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.yappa.clientsabm.model.Client;
+import com.yappa.clientsabm.repository.ClientRepository;
+import com.yappa.clientsabm.service.ClientService;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+
+@Service
+public class ClientServiceImpl implements ClientService{
+
+	@Autowired ClientRepository repository;
+	
+	
+	@Override
+	@Transactional
+	public void insert(Client client) {
+		repository.save(client);
+	}
+
+	@Override
+	public Client get(Integer id) {
+		return repository.findById(id)
+				.orElseThrow(() -> new EntityNotFoundException("Client not found with id: " + id));
+	}
+
+	@Override
+	public List<Client> getAll() {
+		return repository.findAll();
+	}
+
+	@Override
+	public List<Client> search(String name) {
+		return repository.findByFirstName(name);
+	}
+
+	@Override
+	public void update(Integer id) {
+		
+		
+	}
+
+}
