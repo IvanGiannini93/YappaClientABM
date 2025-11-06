a81a591de3262b50eb41c22acfb9d4d78e89531a
Giannini <ivan.giannini@bizland.tech>
2025-11-05 22:56:51 -0300

    first commit

---
+++ src/main/java/com/yappa/clientsabm/ClientsAbmApplication.java
@@ -0,0 +1,13 @@
+package com.yappa.clientsabm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientsAbmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientsAbmApplication.class, args);
+	}
+
+}
