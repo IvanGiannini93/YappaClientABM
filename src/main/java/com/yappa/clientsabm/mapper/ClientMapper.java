925c8fc9f0f54a4e6600b4d4f98e61053947f1e7
701792 <701792@host.docker.internal>
2025-11-06 19:21:07 -0300

    YAP-006: configure database conection

--- src/main/java/com/yappa/clientsabm/mapper/ClientMapper.java
+++ src/main/java/com/yappa/clientsabm/mapper/ClientMapper.java
@@ -2,9 +2,11 @@ package com.yappa.clientsabm.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
+import org.springframework.stereotype.Component;
 
 import com.yappa.clientsabm.dto.ClientDto;
 import com.yappa.clientsabm.model.Client;
+
 
 @Mapper(componentModel = "spring")
 public interface ClientMapper {
